b8c4ba359831b896a0f8b8d6bf4bee08db17c11d
Mario Rodriguez <Mario_Rodriguez_36@progressive.com>
2023-05-01 16:41:59 -0400

    End of day push!

---
+++ ProductsSolution/ProductsApi.IntegrationTests/Demo/GetTests.cs
@@ -0,0 +1,46 @@
+﻿using Alba;
+using Microsoft.Extensions.DependencyInjection;
+using ProductsApi.Demo;
+
+namespace ProductsApi.IntegrationTests.Demo;
+
+public class GetTests
+{
+    [Fact]
+    public async Task Get200StatusCode()
+    {
+        await using var host = await AlbaHost.For<Program>(options =>
+        {
+            options.ConfigureServices((context, sp) =>
+            {
+                sp.AddSingleton<ISystemClock, FakeTestingClock>();
+            });
+        });
+
+        var expectedResponse = new DemoResponse
+        {
+            Message = "Hello from the other side!",
+            CreatedAt = new DateTimeOffset(new DateTime(1969, 4, 20, 23, 59, 00), TimeSpan.FromHours(-4))
+        };
+
+        // Scenarios
+        var response = await host.Scenario(api =>
+        {
+            api.Get.Url("/demo");
+            api.StatusCodeShouldBeOk();
+            api.Header("content-type").ShouldHaveValues("application/json; charset=utf-8");
+        });
+
+        var actualResponse = response.ReadAsJson<DemoResponse>();
+
+        Assert.Equal(expectedResponse, actualResponse);
+    }
+}
+
+public class FakeTestingClock : ISystemClock
+{
+    public DateTimeOffset GetCurrent()
+    {
+        return new DateTimeOffset(new DateTime(1969, 4, 20, 23, 59, 00), TimeSpan.FromHours(-4));
+    }
+}
